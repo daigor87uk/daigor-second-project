9774e876bfce3247561259cc84e255816e6c2d18
daigor <daigor.rodrigues@gmail.com>
2022-04-25 12:26:42 +0100

    ftbranch

--- src/main/java/com/qa/daigorsecondproject/CMMS.java
+++ src/main/java/com/qa/daigorsecondproject/CMMS.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DaigorSecondProjectApplication {
+public class CMMS {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DaigorSecondProjectApplication.class, args);
+		SpringApplication.run(CMMS.class, args);
 	}
 
 }
